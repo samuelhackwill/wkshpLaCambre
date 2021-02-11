ad87a290203dff2c4d9e9dc2b16b88c98296d183
Samuel <hackmew@gmail.com>
2021-02-11 12:29:36 +0100

    Add comments and links to doc

--- client/main.js
+++ client/main.js
@@ -9,12 +9,18 @@ Template.hello.onCreated(function helloOnCreated() {
 });
 
 Template.hello.helpers({
+// METEOR BLAZE : https://guide.meteor.com/v1.2/blaze
 
+	// this helper returns all the objects from the database
 	eleves:function(){
+// JS RETURN : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/return
 		return Eleves.find({})
 	},
 
+	// this helper returns a css opacity value 
+	// if the object has a hidden=true parameter
 	opacityChanger:function(){
+// JS IF : https://www.w3schools.com/js/js_if_else.asp
 		if (this.hidden==true) {
 			return 0.1
 		}else{
@@ -25,20 +31,29 @@ Template.hello.helpers({
 });
 
 Template.hello.events({
+// METEOR EVENTS : http://meteortips.com/first-meteor-tutorial/events/
+
 
 	"click .clickable":function(event){
+// JS CONSOLE.LOG : https://developer.mozilla.org/en-US/docs/Web/API/Console/log
 		console.log(event.currentTarget.innerHTML)
 
 		// get the _id of the database object
-		// corresponding to the name i've clicked
-		// on
+		// corresponding to the name i've clicked on
+		// (we're going to need it when we try to update the db)
+// MONGODB SELECTORS : https://docs.meteor.com/api/collections.html#selectors
 		tempEleve = Eleves.findOne({
 			"name":event.currentTarget.innerHTML})
+
+		// if hidden was true, make it false
+		// and vice versa (this is what =! does)
+// JS BOOLEANS : https://www.w3schools.com/js/js_booleans.asp
 		switchStatus =! tempEleve.hidden
 
+		// update hidden with the new value
+// METEOR UPDATE : https://docs.meteor.com/api/collections.html#Mongo-Collection-update
 		Eleves.update(tempEleve._id, 
 			{$set:{"hidden":switchStatus}
 		})
 	}
-
 });
--- server/main.js
+++ server/main.js
@@ -7,6 +7,7 @@ Eleves = new Mongo.Collection("eleves")
 
 // this is all the items to
 // be inserted in the database.
+// JS ARRAY : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
 ElevesArray = ["Julie", "Thomas", "Maxime", "Dariusz", "Thimotee", "Yulen", "Pierre", "Samuel"]
 
 Meteor.startup(() => {
@@ -15,13 +16,14 @@ Meteor.startup(() => {
   // if the datarstrhbe is empty
   if(Eleves.find({}).fetch()==false){
   	// log "the database is empty"
+// JS LOOPS : https://www.w3schools.com/js/js_loop_for.asp
 	for (var i =0; i < ElevesArray.length; i++) {
 		console.log("the database is empty")
 		Eleves.insert({"name":ElevesArray[i], "hidden":false})
 	}
 	// if the database is NOT empty
   }else{
-  	// log "the database is full"
+  // log "the database is full"
 	console.log("the database is already full")
   }
 });
